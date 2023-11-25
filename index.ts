e3acb03be4d3743d533286950e5b17f32b59a1af
Ayato <ay4toh5i@gmail.com>
2023-11-25 23:27:33 +0900

    Add wrapper logic for sqltools/formatter

--- index.ts
+++ index.ts
@@ -1,6 +1,44 @@
+import { parseArgs } from "https://deno.land/std@0.207.0/cli/mod.ts";
+import { expandGlob } from "https://deno.land/std@0.207.0/fs/mod.ts";
 import formatter from "npm:@sqltools/formatter@1.2.5";
 
-console.log(formatter.format("select * from users where id = 1", {
-  language: "sql",
-  reservedWordCase: "upper",
-}));
+function parseInteger(i: string | undefined, defaultValue: number): number {
+  if (i === undefined) {
+    return defaultValue;
+  }
+
+  const n = Number(i);
+
+  if (isNaN(n)) {
+    throw new Error("value must be integer.");
+  }
+
+  return n;
+}
+
+const flags = parseArgs(Deno.args);
+
+const options: Parameters<typeof formatter.format>[1] = {
+  language: flags["language"] ?? "sql",
+  indent: flags["indent"],
+  reservedWordCase: flags["reservedWordCase"] ?? "upper",
+  linesBetweenQueries: parseInteger(flags["linesBetweenQueries"], 2),
+};
+
+const targets = flags._;
+
+for (const target of targets) {
+  if (typeof target !== "string") {
+    console.error("target must be specified.");
+    Deno.exit(1);
+  }
+
+  for await (const entry of expandGlob(target)) {
+    console.log(entry.path);
+    const text = await Deno.readTextFile(entry.path);
+
+    const formatted = formatter.format(text, options);
+
+    await Deno.writeTextFile(entry.path, formatted);
+  }
+}
